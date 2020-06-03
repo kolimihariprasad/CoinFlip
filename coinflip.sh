5649298abafa79f80c1c56b4ba6f06b50cb687bc
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-06-03 12:02:06 +0530

    Added: Full program

--- coinflip.sh
+++ coinflip.sh
@@ -1,9 +1,37 @@
-heads=0
-tails=0
-flip=$((RANDOM%2))
-if [ $flip -eq 1 ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+declare -A coinflips
+declare -A order
+read -p "enter the no of times coin needs to be fliped" flips
+echo "enter the choice "
+echo "1.single"
+echo "2.double"
+echo "3.Triple"
+read choice
+for((i=1;i<=$flips;i++))
+do
+        sides=""
+        for((j=1;j<=$choice;j++))
+        do
+                flip=$((RANDOM%2))
+                if [ $flip -eq 1 ]
+                then
+                        side="H"
+                else
+                        side="T"
+                fi
+                sides=$sides$side
+        done
+        order[$i]=$sides
+        coinflips[$sides]=$(( ${coinflips[$sides]}+1 ))
+done
+max=0
+for((k=1;k<=$flips;k++))
+do
+        key=${order[$k]}
+        if [ ${coinflips[$key]} -gt $max ]
+        then
+                max=${coinflips[$key]}
+                combination=$key
+        fi
+done
+percentage=`awk "BEGIN {print ($max/$flips)*100}"`
+echo "Winning combination is" $combination "-" $percentage"%"
